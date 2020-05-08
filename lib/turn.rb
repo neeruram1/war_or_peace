52eb64e909b922ae870c800332655d82aee4569f
“Neeru <neeram85@gmail.com>
2020-05-08 08:16:40 -0600

    Added winner method to Turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,6 +7,19 @@ attr_reader :player1, :player2, :spoils_of_war
     @spoils_of_war = []
   end
 
-
-
+  def type
+    if @spoils_of_war = [] || @spoils_of_war.count == 2
+      return :basic
+    else
+      return nil
+    end
   end
+
+  def winner
+    if @player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+      @player1
+    else
+      @player2
+    end
+  end
+end
