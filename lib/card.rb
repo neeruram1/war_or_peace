211eee47a74d1bf2d4bc06f5caf39dfb3446cdbb
“Neeru <neeram85@gmail.com>
2021-05-12 17:50:20 -0600

    restarting project

--- lib/card.rb
+++ lib/card.rb
@@ -1,11 +1,1 @@
-class Card
-  attr_reader :suit,
-              :value,
-              :rank
 
-  def initialize(suit, value, rank)
-    @suit = suit
-    @value = value
-    @rank = rank
-  end
-end
