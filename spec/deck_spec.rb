38947793ae217f308da082f641149b1e3ca1f7bb
“Neeru <neeram85@gmail.com>
2021-05-12 18:55:55 -0600

    create deck spec file and tests

---
+++ spec/deck_spec.rb
@@ -0,0 +1,31 @@
+require './lib/deck'
+
+RSpec.describe Deck do
+  before(:each) do
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @cards = [@card1, @card2, @card3]
+    @deck = Deck.new(@cards)
+  end
+  it "exists" do
+    expect(@deck).to be_an_instance_of(Deck)
+  end
+
+  it "has readable attributes" do
+    expect(@deck.cards).to eq @cards
+  end
+
+  it "can return the rank of a card" do
+    expect(@deck.rank_of_card_at(0)).to eq 12
+    expect(@deck.rank_of_card_at(2)).to eq 14
+  end
+
+  it "can return high ranking cards" do
+    expect(@deck.high_ranking_cards).to eq([@card1, @card3])
+  end
+
+  it "can return the percentage of high ranking cards" do
+    expect(@deck.percent_high_ranking).to eq 66.67
+  end
+end
