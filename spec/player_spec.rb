2ce0fc03dc5717ca3312da12d7398ec52fd6dbed
“Neeru <neeram85@gmail.com>
2021-05-12 19:21:37 -0600

    Write test for player1 to see if exists

---
+++ spec/player_spec.rb
@@ -0,0 +1,15 @@
+require './lib/player'
+
+RSpec.describe Player do
+  before(:each) do
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @cards = [@card1, @card2, @card3]
+    @deck = Deck.new(@cards)
+    @player1 = Player.new(@deck)
+  end
+
+  it "exists" do
+    expect(@player1).to be_an_instance_of(Player)
+  end
